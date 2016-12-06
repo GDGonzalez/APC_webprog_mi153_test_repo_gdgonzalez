faa54ae90a8f683f215b504acf067f3de44b26cd
Gabriel D. Gonzalez <gabdgonzalez@gmail.com>
2016-12-06 13:17:42 +0800

    Edited the <html> tag; copied the <html> tag from index.php

--- Exercise5/mypage.php
+++ Exercise5/mypage.php
@@ -12,7 +12,7 @@ if(isset($_GET['delete_id']))
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta charset="utf-8">
         <title>Project: Blog</title>
